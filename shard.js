982b36066f9316960dcb4c7d1ad504bf13175ec9
Arypton <104511692+AryptonXD@users.noreply.github.com>
2024-07-06 12:52:01 +0530

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,16 +1,15 @@
 const { ClusterManager, HeartbeatManager } = require('discord-hybrid-sharding');
 const config = require("./src/config/config.json");
+const { availableParallelism } = require('os');
 
 const manager = new ClusterManager(`./src/index.js`, {
   totalShards: 'auto',
-  shardsPerClusters: 3,
-  totalClusters: 'auto',
+  shardsPerClusters: 4,
+  totalClusters: availableParallelism(),
   respawn: true,
   mode: 'worker',
   token: config.token,
 });
-
-manager.spawn().catch(console.error);
 
 manager.extend(
   new HeartbeatManager({
@@ -18,3 +17,33 @@ manager.extend(
     maxMissedHeartbeats: 5,
   })
 );
+
+manager.on('clusterCreate', cluster => {
+  console.log(`Launched cluster ${cluster.id}`);
+  monitorCluster(cluster);
+});
+
+async function monitorCluster(cluster) {
+  cluster.on('ready', () => console.log(`Cluster ${cluster.id} is ready`));
+  cluster.on('disconnect', () => console.log(`Cluster ${cluster.id} disconnected`));
+  cluster.on('reconnecting', () => console.log(`Cluster ${cluster.id} reconnecting`));
+  cluster.on('death', process => {
+    console.error(`Cluster ${cluster.id} died with exit code ${process.exitCode}, signal ${process.signalCode}`);
+    restartCluster(cluster);
+ });
+}
+
+async function restartCluster(cluster) {
+  try {
+    await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log(`Restarting cluster ${cluster.id}`);
+    await cluster.respawn();
+    console.log(`Cluster ${cluster.id} restarted`);
+    monitorCluster(cluster);
+  } catch (err) {
+    console.error(`Failed to restart cluster ${cluster.id}: ${err}`);
+    restartCluster(cluster);
+  }
+}
+
+manager.spawn().catch(console.error);
